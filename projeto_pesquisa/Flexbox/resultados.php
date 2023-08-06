42e707ade5a33da8b749409089e840eaf7ce1d0f
Mateus Santos <mateussilvanzg@gmail.com>
2023-08-06 17:03:37 -0300

    Atualizações no projeto

--- projeto_pesquisa/Flexbox/resultados.php
+++ projeto_pesquisa/Flexbox/resultados.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
+    <title>Resultado</title>
 </head>
 <body>
+    <?php
     
+    ?>
 </body>
 </html>
